47d786030338da5e489c648a74113360d6c1de3d
AvaterClasher <avater.clasher@gmail.com>
2024-07-01 21:01:31 +0530

    style: formatted

--- app/_components/navbar-comp/Navbar.tsx
+++ app/_components/navbar-comp/Navbar.tsx
@@ -3,11 +3,15 @@ import { AlignLeft, SquarePen } from "lucide-react";
 export default function Navbar() {
   return (
     <nav className="mx-5 mt-6 flex justify-between">
-      <button type="button"><AlignLeft /></button>
+      <button type="button">
+        <AlignLeft />
+      </button>
       <button type="button" className="text-bold">
         lawson.
       </button>
-      <button type="button"><SquarePen /></button>
+      <button type="button">
+        <SquarePen />
+      </button>
     </nav>
   );
 }
