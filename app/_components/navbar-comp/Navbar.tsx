70f37e5d2c202fad89772bb5338c355b8e793e25
AvaterClasher <avater.clasher@gmail.com>
2024-07-01 18:57:47 +0530

    feat: added navbar

---
+++ app/_components/navbar-comp/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+	return (
+		<nav className="flex justify-between mx-2 mt-3">
+			<button type="button">Hello</button>
+            <button type="button" className="text-bold">lawson.</button>
+            <button type="button">Hello</button>
+		</nav>
+	);
+}
