ad0d71142e687cdcb457524d95a3c621c2e0477d
AvaterClasher <avater.clasher@gmail.com>
2024-07-01 20:49:06 +0530

    feat: added icons for list and new chat

--- app/_components/navbar-comp/Navbar.tsx
+++ app/_components/navbar-comp/Navbar.tsx
@@ -1,11 +1,13 @@
+import { AlignLeft, SquarePen } from "lucide-react";
+
 export default function Navbar() {
   return (
-    <nav className="mx-2 mt-3 flex justify-between">
-      <button type="button">Hello</button>
+    <nav className="mx-5 mt-6 flex justify-between">
+      <button type="button"><AlignLeft /></button>
       <button type="button" className="text-bold">
         lawson.
       </button>
-      <button type="button">Hello</button>
+      <button type="button"><SquarePen /></button>
     </nav>
   );
 }
