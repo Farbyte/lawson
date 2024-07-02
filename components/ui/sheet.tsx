642bdc2eb55ef625d08a45c07f8151129b92df32
AvaterClasher <avater.clasher@gmail.com>
2024-07-02 12:08:44 +0530

    chore: remove unused import

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
-import { AlignLeft, SquarePen } from "lucide-react"
+import { AlignLeft } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
