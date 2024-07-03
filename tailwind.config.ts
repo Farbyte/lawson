e281a1babd03188326e56e7d4d3919cf4cef8507
AvaterClasher <avater.clasher@gmail.com>
2024-07-03 19:20:12 +0530

    feat: added uploadthing tailwind styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import { withUt } from "uploadthing/tw"
 
 const config = {
   darkMode: ["class"],
@@ -37,4 +38,4 @@ const config = {
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
-export default config
+export default withUt(config)
