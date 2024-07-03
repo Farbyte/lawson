9a28728db75aeaffa1bcf941ee3eff6bcec86cc2
AvaterClasher <avater.clasher@gmail.com>
2024-07-03 19:18:44 +0530

    feat: setup uploadthing routes

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,31 @@
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
+import { UploadThingError } from 'uploadthing/server';
+
+const f = createUploadthing();
+
+const auth = (req: Request) => ({ id: 'fakeId' }); // Fake auth function
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const user = await auth(req);
+
+      // If you throw, the user will not be able to upload
+      if (!user) throw new UploadThingError('Unauthorized');
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log('Upload complete for userId:', metadata.userId);
+
+      console.log('file url', file.url);
+
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
