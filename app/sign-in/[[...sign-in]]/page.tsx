ca3ffaa744822e806e98975dd448e0328121497b
AvaterClasher <avater.clasher@gmail.com>
2024-07-03 19:47:55 +0530

    feat: updated the sign in and sign up page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,19 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <div className="flex justify-center py-24">
-      <SignIn />
+    <div className="mt-5">
+      <div className="flex mx-auto justify-center items-center mt-10">
+        <SignIn
+          appearance={{
+            elements: {
+              formButtonPrimary:
+                'bg-black hover:bg-gray-700 transition text-sm normal-case',
+            },
+          }}
+          forceRedirectUrl="/chat"
+        />
+      </div>
     </div>
   );
 }
