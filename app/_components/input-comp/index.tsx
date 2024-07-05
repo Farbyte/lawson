2faf48364b46010e9453fa74edf792a585eb54aa
AvaterClasher <avater.clasher@gmail.com>
2024-07-05 10:53:15 +0530

    fix: removed the height increase logic

--- app/_components/input-comp/index.tsx
+++ app/_components/input-comp/index.tsx
@@ -6,23 +6,6 @@ import { CornerRightUp, Plus } from "lucide-react";
 import { UploadButton } from "../uploadthing";
 
 export const InputComp = () => {
-  const [value, setValue] = useState("");
-  const [height, setHeight] = useState("40px");
-  const [overflow, setOverflow] = useState("auto");
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-
-  // Needs input re-rendering the api route too many times
-
-  // const handleInput = (event: any) => {
-  //   const textareaLineHeight = 40;
-  //   const { target } = event;
-  //   setValue(target.value);
-  //   setHeight(`${target.scrollHeight}px`);
-
-  //   if (target.value === "") {
-  //     setHeight(`${textareaLineHeight}px`);
-  //   }
-  // };
 
   const Uploadbutton = () => (
     <UploadButton
@@ -45,29 +28,15 @@ export const InputComp = () => {
     />
   );
 
-  // useEffect(() => {
-  //   if (textareaRef.current) {
-  //     if (textareaRef.current.scrollHeight < 80) {
-  //       setHeight(`${textareaRef.current.scrollHeight}px`);
-  //     } else {
-  //       setOverflow(`overflow-y-auto`);
-  //     }
-  //   }
-  // }, [textareaRef]);
-
   return (
     <div className="flex items-center justify-center">
       <div className="fixed bottom-6 w-full max-w-2xl">
         <form className="relative m-auto flex items-center gap-5 overflow-y-auto rounded-[26px] bg-[#2F2F2F] px-3 text-base md:px-5 lg:px-1 xl:px-5">
           <Uploadbutton />
           <Textarea
-            ref={textareaRef}
             className="min-w-0 flex-1 resize-none overflow-y-auto bg-[#2F2F2F] p-2 text-white placeholder-white outline-none focus:ring-0"
             placeholder="Ask me anything..."
             rows={1}
-            style={{ height, overflow }}
-            // value={value}s
-            // onInput={handleInput}
           />
           <button
             type="submit"
