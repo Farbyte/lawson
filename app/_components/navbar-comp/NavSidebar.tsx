178ea30b1986bada3b0e57618f6f7b6daf416cf9
AvaterClasher <avater.clasher@gmail.com>
2024-07-05 12:26:52 +0530

    feat: added navbar mapping from database

--- app/_components/navbar-comp/NavSidebar.tsx
+++ app/_components/navbar-comp/NavSidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Sheet,
   SheetContent,
@@ -6,8 +8,19 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { AlignLeft } from "lucide-react";
+import { useState } from "react";
 
-export default function NavSidebar() {
+interface Document {
+  id: string;
+  fileName: string;
+  fileUrl: string;
+  createdAt: Date;
+}
+
+export default function NavSidebar({ initialDocs }: { initialDocs: Document[] }) {
+
+  const [docs, setDocs] = useState(initialDocs);
+
   return (
     <>
       <Sheet>
@@ -17,8 +30,10 @@ export default function NavSidebar() {
         <SheetContent>
           <SheetHeader>
             <SheetDescription className="mt-[60px] flex flex-col items-center justify-center">
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              <div>Documents</div>
+              {docs.map((doc) => (
+                <div key={doc.id}>{doc.fileName}</div>
+              ))}
             </SheetDescription>
           </SheetHeader>
         </SheetContent>
--- app/_components/navbar-comp/Navbar.tsx
+++ app/_components/navbar-comp/Navbar.tsx
@@ -1,11 +1,24 @@
 import { AlignLeft, SquarePen } from "lucide-react";
 import NavSidebar from "./NavSidebar";
 import { UserButton } from "@clerk/nextjs";
+import { currentUser, User } from "@clerk/nextjs/server";
+import prisma from "@/utils/prisma";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user: User | null = await currentUser();
+
+  const docsList = await prisma.document.findMany({
+    where: {
+      userId: user?.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <nav className="mx-5 mt-6 flex justify-between">
-      <NavSidebar />
+      <NavSidebar initialDocs={docsList} />
       <button type="button" className="text-bold">
         lawson.
       </button>
