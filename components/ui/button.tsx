f44a8a7ca06de945368162825a5eaf12d465e746
AvaterClasher <avater.clasher@gmail.com>
2024-07-11 20:46:30 +0530

    feat: added dark mode

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,11 +5,11 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-[#2F2F2F] dark:text-white dark:hover:bg-[#2F2F2F]/30",
+        default: "bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-[#212121] dark:text-white dark:hover:bg-[#2F2F2F]",
         destructive:
           "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
         outline:
