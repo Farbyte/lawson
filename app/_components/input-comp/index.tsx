ce59d263b541876bc04a94a9431a58fb5fca6ae2
AvaterClasher <avater.clasher@gmail.com>
2024-07-11 22:42:38 +0530

    style(light-mode): input comps

--- app/_components/input-comp/index.tsx
+++ app/_components/input-comp/index.tsx
@@ -50,11 +50,11 @@ export const InputComp = ({
       <div className="fixed bottom-2 w-full max-w-2xl">
         {activeTab !== "summary" ? (
           <form
-            className="relative m-auto flex items-center gap-5 overflow-y-auto rounded-[26px] bg-[#2F2F2F] px-3 text-base md:px-5 lg:px-1 xl:px-5"
+            className="relative m-auto flex items-center gap-5 overflow-y-auto rounded-[26px] bg-[#F4F4F4] dark:bg-[#2F2F2F] px-3 text-base md:px-5 lg:px-1 xl:px-5"
             onSubmit={(e) => handleSub(e)}
           >
             <Textarea
-              className="min-w-0 flex-1 resize-none overflow-y-auto bg-[#2F2F2F] p-2 text-white placeholder-white outline-none focus:ring-0"
+              className="min-w-0 flex-1 resize-none overflow-y-auto bg-[#F4F4F4] dark:bg-[#2F2F2F] p-2 text-black outline-none focus:ring-0"
               placeholder={
                 isLoading ? "Waiting for response..." : "Ask me anything..."
               }
@@ -71,27 +71,28 @@ export const InputComp = ({
             <button
               type="submit"
               disabled={isLoading}
-              className="flex rounded-sm border-none bg-transparent text-white transition duration-300 ease-in-out"
+              className="flex rounded-sm border-none bg-transparent text-black transition duration-300 ease-in-out"
             >
               <CornerRightUp className="h-6 w-6" />
             </button>
           </form>
         ) : (
           <div className="relative flex min-w-0 max-w-2xl items-center justify-center gap-1 rounded-[26px] text-base">
-            <Button onClick={() => handlefetchSummary("large", "")}>
+            <Button onClick={() => handlefetchSummary("large", "")} className="bg-slate-100">
               Large Summary
             </Button>
-            <Button onClick={() => handlefetchSummary("small", "")}>
+            <Button onClick={() => handlefetchSummary("small", "")} className="bg-slate-100">
               Small Summary
             </Button>
             <Button
               onClick={() => handlefetchSummary("custom", "Our custom prompt")}
+              className="bg-slate-100"
             >
               Custom Summary
             </Button>
           </div>
         )}
-        <p className="mt-4 text-center text-xs text-[#838E94]">
+        <p className="mt-4 text-center text-xs text-gray-700 dark:text-[#838E94]">
           Doc ID : {extractId(pathname)}
         </p>
       </div>
