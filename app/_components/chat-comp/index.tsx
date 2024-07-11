8ee7ed2b4d2dc3a79e5ac8ada663ef3c9cff0bc4
AvaterClasher <avater.clasher@gmail.com>
2024-07-11 12:16:30 +0530

    feat: added styling to chat component and added bot image

--- app/_components/chat-comp/index.tsx
+++ app/_components/chat-comp/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from "react";
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import { useTabsStore } from "@/app/_store/tabsStore";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface ChatCompProps {
   isLoading: boolean;
@@ -25,57 +26,58 @@ export const ChatComp = ({
 
   return (
     activeTab === "chat" && (
-      <div className="align-center flex h-[90vh] w-full flex-col justify-between">
+      <div className="align-center mx-auto flex h-[75vh] w-[80vw] flex-col justify-between pb-[9rem]">
         <div
-          className={`bg-background no-scrollbar flex h-[80vh] min-h-min w-full items-center justify-center sm:h-[85vh]`}
+          className={`bg-background flex h-[77vh] w-full items-center justify-center sm:h-[77vh]`}
         >
           <div
             ref={messageListRef}
-            className="mt-4 h-full w-full overflow-y-scroll rounded-md"
+            className="mt-4 h-full w-full overflow-y-scroll rounded-md scrollbar-hide"
           >
             {messages.length === 0 && (
               <div className="flex h-full items-center justify-center text-xl">
-                Ask your first question below!
+                Ask your first question below !
               </div>
             )}
-            
-            {messages.map((message, index) => {
-              const sources = sourcesForMessages[index] || undefined;
-              const isLastMessage = !isLoading && index === messages.length - 1;
-              const previousMessage = index !== messages.length - 1
-              return (
-                <div key={`chatMessage-${index}`}>
-                  <div
-                    className={`text-foreground animate p-4 ${
-                      message.role === "assistant"
-                        ? "bg-background/50"
-                        : isLoading && index === messages.length - 1
-                          ? "bg-background animate-pulse"
-                          : "bg-background"
-                    }`}
-                  >
-                    <div className="flex">
-                      <img
-                        key={index}
-                        src={
-                          message.role === "assistant"
-                            ? "/bot-icon.png"
-                            : userProfilePic
-                        }
-                        alt="profile image"
-                        width={message.role === "assistant" ? 35 : 33}
-                        height={30}
-                        className="mr-4 h-full rounded-sm"
-                        // priority
-                      />
-                      <ReactMarkdown className="prose">
-                        {message.content}
-                      </ReactMarkdown>
+            <ScrollArea>
+              {messages.map((message, index) => {
+                const sources = sourcesForMessages[index] || undefined;
+                const isLastMessage =
+                  !isLoading && index === messages.length - 1;
+                const previousMessage = index !== messages.length - 1;
+                return (
+                  <div key={`chatMessage-${index}`}>
+                    <div
+                      className={`text-foreground animate p-4 ${
+                        message.role === "assistant"
+                          ? "bg-background/50"
+                          : isLoading && index === messages.length - 1
+                            ? "bg-background animate-pulse"
+                            : "bg-background"
+                      }`}
+                    >
+                      <div className="flex">
+                        <img
+                          key={index}
+                          src={
+                            message.role === "assistant"
+                              ? "/bot.png"
+                              : userProfilePic
+                          }
+                          alt="profile image"
+                          width={message.role === "assistant" ? 33 : 33}
+                          height={30}
+                          className="mr-4 h-full rounded-sm"
+                        />
+                        <ReactMarkdown className="prose">
+                          {message.content}
+                        </ReactMarkdown>
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </ScrollArea>
           </div>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,7 +35,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),require('tailwind-scrollbar-hide')],
 } satisfies Config
 
 export default withUt(config)
