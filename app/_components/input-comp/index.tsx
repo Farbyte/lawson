ec30a2430adb7621d1c359e269ba19c976528113
AvaterClasher <avater.clasher@gmail.com>
2024-07-11 11:11:05 +0530

    perf: removed the upload button from the input comp

--- app/_components/input-comp/index.tsx
+++ app/_components/input-comp/index.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import { Textarea } from "@/components/ui/textarea";
-import { CornerRightUp, Plus } from "lucide-react";
-import { UploadButton } from "../uploadthing";
-import { uploadPdf } from "@/app/_hooks/uploadDocs";
-import { useUser } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
-import { getLatestDocId } from "@/app/_hooks/getLatest";
+import { CornerRightUp } from "lucide-react";
+import { usePathname } from "next/navigation";
 import { useTabsStore } from "@/app/_store/tabsStore";
 import { Button } from "@/components/ui/button";
 
@@ -34,38 +30,9 @@ export const InputComp = ({
   textAreaRef,
   error,
 }: InputCompProps) => {
-  const user = useUser();
-
-  const router = useRouter();
-
   const pathname = usePathname();
 
   const { activeTab } = useTabsStore();
-
-  const Uploadbutton = () => (
-    <UploadButton
-      className="ut-allowed-content:hidden"
-      endpoint="pdfUploader"
-      content={{
-        button({ ready }) {
-          if (ready) return <Plus className="h-5 w-5" />;
-          else return <Plus className="h-5 w-5" />;
-        },
-      }}
-      appearance={{ button: "bg-transparent w-6 h-6" }}
-      config={{ mode: "auto" }}
-      onClientUploadComplete={async (res) => {
-        await uploadPdf(res[0].url, res[0].name);
-        const userId = await user.user?.id;
-        const docId = await getLatestDocId(userId);
-        router.push(`/chat/summary/${docId}`);
-        console.log("Files: ", res[0].url);
-      }}
-      onUploadError={(error: Error) => {
-        alert(`ERROR! ${error.message}`);
-      }}
-    />
-  );
 
   function extractId(url: string): string {
     const check = url.substring(0,14);
@@ -86,7 +53,6 @@ export const InputComp = ({
             className="relative m-auto flex items-center gap-5 overflow-y-auto rounded-[26px] bg-[#2F2F2F] px-3 text-base md:px-5 lg:px-1 xl:px-5"
             onSubmit={(e) => handleSub(e)}
           >
-            <Uploadbutton />
             <Textarea
               className="min-w-0 flex-1 resize-none overflow-y-auto bg-[#2F2F2F] p-2 text-white placeholder-white outline-none focus:ring-0"
               placeholder={
--- app/_components/input-comp/rootInput.tsx
+++ app/_components/input-comp/rootInput.tsx
@@ -1,44 +1,11 @@
 "use client";
 
 import { Textarea } from "@/components/ui/textarea";
-import { CornerRightUp, Plus } from "lucide-react";
-import { UploadButton } from "../uploadthing";
-import { uploadPdf } from "@/app/_hooks/uploadDocs";
-import { useUser } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
-import { getLatestDocId } from "@/app/_hooks/getLatest";
+import { CornerRightUp } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export const RootInput = () => {
-  const user = useUser();
-
-  const router = useRouter();
-
   const pathname = usePathname();
-
-  const Uploadbutton = () => (
-    <UploadButton
-      className="ut-allowed-content:hidden"
-      endpoint="pdfUploader"
-      content={{
-        button({ ready }) {
-          if (ready) return <Plus className="h-5 w-5" />;
-          else return <Plus className="h-5 w-5" />;
-        },
-      }}
-      appearance={{ button: "bg-transparent w-6 h-6" }}
-      config={{ mode: "auto" }}
-      onClientUploadComplete={async (res) => {
-        await uploadPdf(res[0].url, res[0].name);
-        const userId = await user.user?.id;
-        const docId = await getLatestDocId(userId);
-        router.push(`/chat/summary/${docId}`);
-        console.log("Files: ", res[0].url);
-      }}
-      onUploadError={(error: Error) => {
-        alert(`ERROR! ${error.message}`);
-      }}
-    />
-  );
 
   function extractId(url: string): string {
     const id = url.substring(14);
@@ -49,7 +16,6 @@ export const RootInput = () => {
     <div className="flex flex-col items-center justify-center">
       <div className="fixed bottom-2 w-full max-w-2xl">
         <form className="relative m-auto flex items-center gap-5 overflow-y-auto rounded-[26px] bg-[#2F2F2F] px-3 text-base md:px-5 lg:px-1 xl:px-5">
-          <Uploadbutton />
           <Textarea
             className="min-w-0 flex-1 resize-none overflow-y-auto bg-[#2F2F2F] p-2 text-white placeholder-white outline-none focus:ring-0"
             placeholder="Ask me anything..."
