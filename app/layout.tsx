4bae8fed87b6bb6be6a449c388e429ab6a5b0fb5
AvaterClasher <avater.clasher@gmail.com>
2024-07-11 19:08:21 +0530

    fix: wrong import for sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
