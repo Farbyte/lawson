78b85e1eff906196d391567613c69191446b8071
AvaterClasher <avater.clasher@gmail.com>
2024-07-13 19:04:24 +0530

    chore(homepage): changed the location of the file

--- app/_home_components/Navbar/navbar.tsx
+++ app/_home_components/Navbar/navbar.tsx
@@ -3,19 +3,18 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 mb-20 shadow-sm dark:bg-black/80">
+    <nav className="sticky top-0 z-50 mb-20 bg-white/90 shadow-sm backdrop-blur-md dark:bg-black/80">
       <div className="] flex items-center justify-between px-5">
         {/* Logo */}
         <div>
           <div className="block md:hidden">{/* Mobile menu */}</div>
         </div>
-        <ul className="hidden space-x-10 py-3 md:flex">
-          <Link href="/docs" className="px-1 py-2">
+        <ul className="hidden space-x-10 py-3 md:flex mr-5">
+          <Link href="/docs" className="py-2">
             Docs
           </Link>
-          <SignInButton children="Login" />
           <div className="rounded-md bg-[#262626] px-3 py-2 text-white">
-            <SignUpButton children="Sign Up" />
+            <SignInButton children="Login" forceRedirectUrl="/chat"/>
           </div>
         </ul>
       </div>
