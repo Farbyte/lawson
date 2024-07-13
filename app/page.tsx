7502d1a7621795f1667aad073b88e2bd02d30867
AvaterClasher <avater.clasher@gmail.com>
2024-07-13 20:53:09 +0530

    feat(homepage): added working comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./_home_components/Navbar/navbar";
 import Hero from "./_home_components/Hero/hero";
 import Features from "./_home_components/Features";
+import Working from "./_home_components/Working";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Features />
+      <Working />
     </main>
   );
 }
