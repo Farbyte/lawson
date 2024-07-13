7085de5653e024eae91fcf9da93ec34cc19180a3
AvaterClasher <avater.clasher@gmail.com>
2024-07-13 21:40:10 +0530

    feat(homepage): completed the working comp

---
+++ app/_home_components/Working/beam.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import React, { forwardRef, useRef } from "react";
+
+import { cn } from "@/lib/utils";
+import { AnimatedBeam } from "@/components/ui/animated-beam";
+import { FileText, Scale, User } from "lucide-react";
+
+const Circle = forwardRef<
+  HTMLDivElement,
+  { className?: string; children?: React.ReactNode }
+>(({ className, children }, ref) => {
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "z-10 flex max-w-4xl items-center justify-center rounded-full border border-[#212121] bg-black p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+});
+
+Circle.displayName = "Circle";
+
+export function Beam({ className }: { className?: string }) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const div1Ref = useRef<HTMLDivElement>(null);
+  const div2Ref = useRef<HTMLDivElement>(null);
+  const div3Ref = useRef<HTMLDivElement>(null);
+  const div4Ref = useRef<HTMLDivElement>(null);
+  const div5Ref = useRef<HTMLDivElement>(null);
+
+  return (
+    <div
+      className={cn(
+        "bg-background relative flex h-[300px] md:h-[500px] w-full items-center justify-center overflow-hidden rounded-lg border border-white/[0.2] p-10 md:shadow-xl",
+        className,
+      )}
+      ref={containerRef}
+    >
+      <div className="flex size-full max-w-4xl flex-row items-stretch justify-between gap-3">
+        <div className="flex flex-col justify-center gap-10 md:gap-[6rem]">
+          <Circle ref={div1Ref}>
+            <FileText className="h-8 w-8 text-white" />
+          </Circle>
+          <Circle ref={div2Ref}>
+            <FileText className="h-8 w-8 text-white" />
+          </Circle>
+          <Circle ref={div3Ref}>
+            <FileText className="h-8 w-8 text-white" />
+          </Circle>
+        </div>
+        <div className="flex flex-col justify-center">
+          <Circle ref={div4Ref} className="size-16">
+            <Scale className="h-8 w-8 text-white" />
+          </Circle>
+        </div>
+        <div className="flex flex-col justify-center">
+          <Circle ref={div5Ref}>
+            <User className="h-8 w-8 text-white" />
+          </Circle>
+        </div>
+      </div>
+
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div1Ref}
+        toRef={div4Ref}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div2Ref}
+        toRef={div4Ref}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div3Ref}
+        toRef={div4Ref}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div4Ref}
+        toRef={div5Ref}
+        curvature={-75}
+        endYOffset={-10}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div5Ref}
+        toRef={div4Ref}
+        curvature={75}
+        endYOffset={10}
+        reverse
+      />
+    </div>
+  );
+}
--- app/_home_components/Working/index.tsx
+++ app/_home_components/Working/index.tsx
@@ -1,7 +1,20 @@
+import { Beam } from "./beam";
+
 export default function Working() {
-    return (
-        <>
-            
-        </>
-    )
+  return (
+    <>
+      <div className="mx-auto mt-40 max-w-7xl px-10 mb-8">
+        <div>
+          <h2 className="text-4xl sm:text-6xl">
+            How does it Work? <br />
+          </h2>
+          <p className="mt-3 max-w-2xl text-lg text-gray-700 dark:text-gray-300 mb-8">
+            Lawson generates its answer based on the pdf and your provided information.
+          </p>
+          <Beam />
+        </div>
+      </div>
+    </>
+  );
 }
+
