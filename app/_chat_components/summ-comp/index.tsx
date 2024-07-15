ea9b601dcdb373178cdce52aeda2e00db0df17d9
AvaterClasher <avater.clasher@gmail.com>
2024-07-15 10:21:19 +0530

    feat: fixed styles of getPdf button

--- app/_chat_components/summ-comp/index.tsx
+++ app/_chat_components/summ-comp/index.tsx
@@ -7,21 +7,22 @@ import { toast } from "sonner";
 import LoadingDots from "./loading";
 import { Button } from "@/components/ui/button";
 import { jsPDF } from "jspdf";
+import { Clipboard } from "lucide-react";
 
-function downloadPDF(content : string){
-    console.log('creating PDF')
-  if (content){
+function downloadPDF(content: string) {
+  console.log("creating PDF");
+  if (content) {
     const doc = new jsPDF({
-      orientation : "portrait",
-      format : "a4"
-    })
-    doc.text(content,10,20,{
-      maxWidth : 190,
-      align : "left"
-    })
-    doc.loadFile
-    doc.save("summaryPDF")
-  } 
+      orientation: "portrait",
+      format: "a4",
+    });
+    doc.text(content, 10, 20, {
+      maxWidth: 190,
+      align: "left",
+    });
+    doc.loadFile;
+    doc.save("summaryPDF");
+  }
 }
 
 export const SummComp = ({
@@ -44,16 +45,24 @@ export const SummComp = ({
           </div>
         )}
         {markdownContent && (
-          <div className="mt-12 flex flex-col items-start">
-            <Button className="my-4" onClick={() => downloadPDF(markdownContent)}>Get PDF</Button>
-            <div className="ml-4 border-b-2">Summary </div>
+          <div className="md:mt-12 mt-2 flex flex-col items-start">
+            <div className="flex items-center justify-between w-full pr-16">
+              <div className="ml-4 border-b-2 text-lg">Summary </div>
+              <button
+                className="my-4 dark:bg-[#4e4c4c] bg-gray-200 text-black hover:bg-gray-300 dark:text-white dark:hover:bg-[#2F2F2F] p-2 rounded-md flex items-center gap-1 text-sm"
+                onClick={() => downloadPDF(markdownContent)}
+              >
+                <Clipboard className="w-4 h-4" />
+                Get PDF
+              </button>
+            </div>
             <ScrollArea className="h-[80vh] w-[80vw] p-4 pb-[6rem] lg:w-[60vw]">
               <ReactMarkdown>{markdownContent}</ReactMarkdown>
             </ScrollArea>
           </div>
         )}
-        {error && toast.error(error, { duration:2000 ,closeButton: true })}
+        {error && toast.error(error, { duration: 2000, closeButton: true })}
       </div>
-    )   
+    )
   );
 };
